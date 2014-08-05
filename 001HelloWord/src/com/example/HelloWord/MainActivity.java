9580def85533b2ef609b47921d792d3cd89eb287
Henry AT <yiperu@gmail.com>
2014-08-05 15:05:30 -0400

    creamos un ejemplo de como hacer un Content provider, para este caso accedimos a los contactos del dispositivo, la idea es mostrarlo en consola

--- 001HelloWord/src/com/example/HelloWord/MainActivity.java
+++ 001HelloWord/src/com/example/HelloWord/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.HelloWord;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
 import android.util.Log;
 
 public class MainActivity extends Activity {
@@ -17,7 +21,23 @@ public class MainActivity extends Activity {
 
         Intent i = new Intent(this, SecondActivity.class);
         i.putExtra("variable01", "Dato de la Variable 01");
-
         startActivity(i);
+
+        access();
+
+
+    }
+
+
+    public void access() {
+        ContentResolver cr = getContentResolver();
+        Cursor cur = cr.query(Contacts.People.CONTENT_URI, null, null, null, null);
+        if(cur.getCount() > 0){
+            while (cur.moveToNext()){
+
+                String name = cur.getString(cur.getColumnIndex(Contacts.People.DISPLAY_NAME));
+                String id = cur.getString(cur.getColumnIndex(Contacts.People._ID));
+            }
+        }
     }
 }
